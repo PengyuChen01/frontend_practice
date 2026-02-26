5991b4dd07cae1bb8d3c57a7ba9be5f6d9fc63d5
pengyu <pengyuchen01@gmail.com>
2026-02-26 13:48:03 -0500

    adding table using material ui

---
+++ frontend_practice/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import DataTable from './DataTable'
+
+function App() {
+  return (
+    <div>
+      <h1>Data Table Example</h1>
+      <DataTable />
+    </div>
+  )
+  
+}
+
+export default App
